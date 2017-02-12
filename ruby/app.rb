29359e83c0f02db15bf4b6b112cf6f7bfd996554
Matt Morley <matt@mpcm.com>
2017-02-11 22:37:34 -0500

    path flattening added

--- ruby/app.rb
+++ ruby/app.rb
@@ -23,15 +23,40 @@ require 'pp'
 
 
 e = Trickle.new()
+# e.debug = true
 e.import 'facts', {
 	'13_82_bil' => 13820000000000,
-	'1321009871' => 'November 11, 2011'
+	'1321009871' => 'November 11, 2011',
+	'weather_1' => 'sunny',
+	'weather_2' => 'rainy',
+	'weather_3' => 'snowy',
 }
 
+e.import 'map', {
+	'alias1' => 'aquarius',
+	'alias2' => 'alias1',
+	'alias3' => 'alias2',
+	'alias4' => 'map.alias1',
+}
+
+# basic path get/set
 e.set 'data.age.of.universe','facts.13_82_bil'
 e.set 'data.age.of.aquarius','facts.1321009871'
 
+# dynamic path resolution
+e.get 'data.age.of.{map.alias1}'             # aquarius
+e.get 'data.age.of.{map.{map.alias2}}'       # aquarius
+e.get 'data.age.of.{map.{map.{map.alias3}}}' # aquarius
+e.get 'data.age.of.{{map.alias4}}'           # aquarius
+
+# set the weather
+e.set 'data.age.of.{map.alias1}_weather','facts.weather_1'
+e.set 'data.age.of.{map.{map.alias2}}_weather','facts.weather_2'
+e.set 'data.age.of.{map.{map.{map.alias3}}}_weather','facts.weather_3'
 
 
 puts "="*50
-pp e.store['data']
+pp e.export['data']
+
+# puts "="*50
+# pp e.export
--- ruby/trickle.rb
+++ ruby/trickle.rb
@@ -12,19 +12,24 @@
 # A op. namespace should be reserved for operations like &&, ||, ==, >=
 
 class Trickle
-  attr_accessor :store
+  attr_accessor :debug
 
   def initialize(store = {})
     @store = store
+    @debug = false
   end
 
-  def get(data_path)
+  def get(path_to_get)
+    self.log "get in #{path_to_get}"
     id = @store.object_id
 
-    # for each segment, get object_id and set into id
-    path_to_set.split('.').each do |segment|
+    # flatten the path into single level segments
+    segments = self.flatten_string( path_to_get ).split('.')
 
-      # binding.pry
+    # for each segment, get object_id and set into id
+    segments.each do |segment|
+
+      # get the object
       a = ObjectSpace._id2ref(id)
 
       # create the segment if not present
@@ -36,15 +41,27 @@ class Trickle
 
     a = ObjectSpace._id2ref(id)
 
+    self.log "get out #{path_to_get} to #{a}"
+    a
+  end
+
+  def flatten_string(s)
+    while s.include?('{')
+      token = s.split('}').first.split('{').last
+      self.log " flat #{token} | #{s}"
+      value = self.get(token)
+      s.gsub!("{#{token}}",value)
+    end
+    return s
   end
 
   def set(path_to_set, path_to_value, value=nil)
-
+    self.log "set in #{path_to_set} to #{path_to_value} #{value}"
     # start with the @data id
     id = @store.object_id
 
     # get the path to, and the tail attr string
-    *segments, tail = path_to_set.split('.')
+    *segments, tail = self.flatten_string( path_to_set ).split('.')
 
     # for each path segment, set id to that object_id
     segments.each do |segment|
@@ -64,25 +81,45 @@ class Trickle
     # assign path a value
     if value.nil?
 
-      # resolve path value
-      value = @store.dig(*path_to_value.split('.'))
-
-      # explode if value is nil... means failed lookup
-      raise ArgumentError, 'path_to_value does not exist at this time' if value.nil?
+      #  get value at path
+      value = self.resolve( self.flatten_string(path_to_value) )
 
     end
 
     # set our path+tail to value
     obj[tail] = value
 
+    self.log "set out #{path_to_set} to #{value}"
+
   end
 
-  def import(data_path, value)
-    self.set(data_path, '', value)
+  def resolve(path_to_value)
+    path = path_to_value.split('.')
+
+    # route rpc calls into functions
+    if path.first == 'rpc'
+      # todo
+    else
+      value = @store.dig(*path)
+    end
+
+    # raise if value is nil... means failed lookup
+    raise ArgumentError, 'path_to_value does not exist at this time' if value.nil?
+
+    value
+  end
+
+  def import(path, value)
+    # imports get set via value, not relative path
+    self.set(path, nil, value)
   end
 
   def export()
     @store
+  end
+
+  def log(msg)
+    puts "  debug: #{msg}" if @debug
   end
 
 end
